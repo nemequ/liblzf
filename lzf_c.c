709765dafaa03a64f332340d061b53c715ceaedd
Marc Lehmann <schmorpforge@schmorp.de>
2007-02-16 22:13:43 +0000

    *** empty log message ***

--- lzf_c.c
+++ lzf_c.c
@@ -84,7 +84,7 @@ unsigned int
 lzf_compress (const void *const in_data, unsigned int in_len,
 	      void *out_data, unsigned int out_len
 #if LZF_STATE_ARG
-              , LZF_STATE *htab
+              , LZF_STATE htab
 #endif
               )
 {
