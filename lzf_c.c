4061b872971bca47293abeebb4880322f0d5bcc9
Marc Lehmann <schmorpforge@schmorp.de>
2010-05-30 05:52:57 +0000

    *** empty log message ***

--- lzf_c.c
+++ lzf_c.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000-2009 Marc Alexander Lehmann <schmorp@schmorp.de>
+ * Copyright (c) 2000-2010 Marc Alexander Lehmann <schmorp@schmorp.de>
  * 
  * Redistribution and use in source and binary forms, with or without modifica-
  * tion, are permitted provided that the following conditions are met:
